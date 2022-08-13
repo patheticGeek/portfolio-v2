c3a35f10ce0541d074dcff3a00195bb3b0005f96
Pathetic Geek <geekpathetic@gmail.com>
2022-08-14 03:36:36 +0530

    ✨ Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [image(), mdx(), sitemap(), tailwind()],
   site: "https://patheticgeek.dev",
+  output: "server",
+  adapter: vercel()
 });
