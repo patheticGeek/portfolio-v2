7c3b3a4f531dbc4b8a4e4cfb702a2c27582b1d6d
Pathetic Geek <geekpathetic@gmail.com>
2022-08-15 19:11:18 +0530

    🐛 Fix tailwind bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [image(), mdx(), sitemap(), tailwind()],
+  integrations: [
+    image(),
+    mdx(),
+    sitemap(),
+    tailwind({ config: { applyBaseStyles: false } }),
+  ],
   site: "https://patheticgeek.dev",
-  markdown: {
-    remarkPlugins: [[rehypeAutolinkHeadings, { behavior: "wrap" }]],
-  },
 });
