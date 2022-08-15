816b181b2facfb7b7d8d8c6d24ccca067893e738
Pathetic Geek <geekpathetic@gmail.com>
2022-08-15 17:41:02 +0530

    📝 site desc update

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,7 @@
 export const SITE_TITLE = "Pathetic Geek";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "Hey there 👋";
 export const GITHUB_USERNAME = "patheticGeek";
 export const GITHUB_TOPIC = "in-portfolio";
-
-export const RESUME_LINK = "PatheticGeekResume.pdf";
 
 export const HOME_LINKS = [
   { label: "📨 Email", href: "mailto:geekpathetic@gmail.com" },
