e1405af1ca1d929a79a51fbaf289e429e1fc3e12
Pathetic Geek <geekpathetic@gmail.com>
2022-08-15 15:24:26 +0530

    🐛 Static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,10 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [image(), mdx(), sitemap(), tailwind()],
   site: "https://patheticgeek.dev",
-  output: "server",
-  adapter: vercel(),
   markdown: {
     remarkPlugins: [[rehypeAutolinkHeadings, { behavior: "wrap" }]],
   },
