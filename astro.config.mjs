f23aed479df7cc699437ee52165212adfb57e093
Pathetic Geek <geekpathetic@gmail.com>
2023-02-01 22:55:32 +0530

    hopefully fix vercel image issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 import { defineConfig } from "astro/config";
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
   integrations: [
-    image(),
     mdx(),
-    tailwind({ config: { applyBaseStyles: false } }),
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
   ],
   site: "https://patheticgeek.dev/",
 });
