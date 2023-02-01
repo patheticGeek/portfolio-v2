0b245e765e6c9d5886567a90ce4f39a48b42787a
Pathetic Geek <geekpathetic@gmail.com>
2023-02-01 21:47:25 +0530

    astro image fix try

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
@@ -11,7 +10,6 @@ export default defineConfig({
   integrations: [
     image(),
     mdx(),
-    sitemap(),
     tailwind({ config: { applyBaseStyles: false } }),
   ],
   site: "https://patheticgeek.dev/",
