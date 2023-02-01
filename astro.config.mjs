f3d90bbd8b6eb3509406b0c8f2e1d7275b5c2fc1
Pathetic Geek <geekpathetic@gmail.com>
2023-02-01 20:22:50 +0530

    Upgrade to astro 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,16 @@ import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/edge";
 
 export default defineConfig({
+  output: "server",
+  adapter: vercel(),
   integrations: [
     image(),
     mdx(),
     sitemap(),
     tailwind({ config: { applyBaseStyles: false } }),
   ],
-  site: "https://patheticgeek.dev",
+  site: "https://patheticgeek.dev/",
 });
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,11 +1,11 @@
-import rss from "@astrojs/rss";
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 
 export const get = async () => {
   return rss({
     title: `Geek's blog`,
     description: `Every sentence is just a remix of the dictionary, and here are my remixes`,
     site: import.meta.env.SITE,
-    items: import.meta.glob("./blog/*.md*"),
+    items: await pagesGlobToRssItems(import.meta.glob("./blog/*.md*")),
     customData: `<language>en-us</language>`,
     stylesheet: "/rss/styles.xsl",
   });
