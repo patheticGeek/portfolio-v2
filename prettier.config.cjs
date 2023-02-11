a0e78bc43044b16259fccbfa7169a14dee6c29ad
Pathetic Geek <geekpathetic@gmail.com>
2023-02-11 17:26:27 +0530

    Fix prettier issue

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,8 +1,11 @@
 module.exports = {
   ...require('prettier-config-standard'),
   tailwindConfig: './tailwind.config.cjs',
-  pluginSearchDirs: ['.'],
-  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+  pluginSearchDirs: [__dirname],
+  plugins: [
+    require.resolve('prettier-plugin-astro'),
+    require.resolve('prettier-plugin-tailwindcss')
+  ],
   overrides: [
     {
       files: '*.astro',
