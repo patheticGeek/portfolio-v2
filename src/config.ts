8c27fd51a50ec79b7d321c00150b4ee98abb32a3
Pathetic Geek <geekpathetic@gmail.com>
2023-09-18 17:27:20 +0530

    small fixes

--- src/config.ts
+++ src/config.ts
@@ -19,6 +19,5 @@ export const NAV_LINKS = [
   { label: 'Home', href: '/' },
   { label: 'Resources', href: '/resources' },
   { label: 'Projects', href: '/projects' },
-  { label: 'Blog', href: '/blog' },
-  { label: 'Resume', href: '/resume' }
+  { label: 'Blog', href: '/blog' }
 ]
