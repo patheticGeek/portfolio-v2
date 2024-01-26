9cff12b17b99264d17e466c3e3439169ee9b21fb
Pathetic Geek <geekpathetic@gmail.com>
2024-01-27 00:36:27 +0530

    trial

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,9 +1,11 @@
 import { readFile } from 'fs/promises'
 
+const pathPrefix = import.meta.env.PROD ? '../../../../src/assets/' : '../../assets'
+
 const fontsToLoad = {
-  firaCode300: readFile(new URL('../assets/fira-code-latin-300-normal.woff', import.meta.url)),
-  firaCode500: readFile(new URL('../assets/fira-code-latin-500-normal.woff', import.meta.url)),
-  firaCode700: readFile(new URL('../assets/fira-code-latin-700-normal.woff', import.meta.url))
+  firaCode300: readFile(new URL(`${pathPrefix}/fira-code-latin-300-normal.woff`, import.meta.url)),
+  firaCode500: readFile(new URL(`${pathPrefix}/fira-code-latin-500-normal.woff`, import.meta.url)),
+  firaCode700: readFile(new URL(`${pathPrefix}/fira-code-latin-700-normal.woff`, import.meta.url))
 }
 
 const fontFilesPromises = await Promise.all(Object.entries(fontsToLoad).map(async ([key, file]) => {
