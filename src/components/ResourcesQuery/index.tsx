37adb768eacfe8690624b20f1005115412920c2c
Pathetic Geek <geekpathetic@gmail.com>
2025-04-21 07:27:48 +0530

    add query page for resources

---
+++ src/components/ResourcesQuery/index.tsx
@@ -0,0 +1,101 @@
+import ms from 'ms'
+import { FormEvent, useCallback, useEffect, useState } from 'react'
+
+const useQuery = () => {
+  const [data, setData] = useState<any>(undefined)
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const query = useCallback(
+    async (input: RequestInfo | URL, init?: RequestInit) => {
+      setLoading(true)
+      try {
+        const result = await fetch(input, init)
+        setData(await result.json())
+        setError(false)
+      } catch (err) {
+        setError(true)
+      } finally {
+        setLoading(false)
+      }
+    },
+    []
+  )
+
+  return { data, error, loading, query }
+}
+
+const LoadingDots = ({ count = 3, interval = 500 }) => {
+  const [current, setCurrent] = useState(0)
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCurrent((c) => (c + 1) % (count + 1))
+    }, interval)
+    return () => clearInterval(id)
+  }, [count, interval])
+
+  return <span>{new Array(current).fill('.').join('')}</span>
+}
+
+const useHydrated = () => {
+  const [hydrated, setHydrated] = useState(false)
+  useEffect(() => setHydrated(true), [])
+  return hydrated
+}
+
+const ResourcesQuery = () => {
+  const hydrated = useHydrated()
+  const { data, error, loading, query } = useQuery()
+
+  const onSubmit = useCallback(
+    (event: FormEvent) => {
+      event.preventDefault()
+      const formData = new FormData(event.currentTarget as any)
+
+      query('/api/bookmark/query', {
+        method: 'POST',
+        body: JSON.stringify({ query: formData.get('query') })
+      })
+    },
+    [loading, query]
+  )
+
+  return (
+    <>
+      <form onSubmit={onSubmit} className="my-4">
+        <input
+          id="query"
+          name="query"
+          placeholder="What are you lookin for today?"
+          className="w-full rounded px-2 py-1 outline-none ring-1 ring-slate-700 transition invalid:ring-2 invalid:ring-slate-500 focus:ring-2 focus:ring-slate-200"
+          type="search"
+          required
+          disabled={loading}
+          autoFocus
+        />
+      </form>
+
+      {loading ? (
+        <p className="query-loading">
+          Geek is thinking very hard
+          <LoadingDots />
+        </p>
+      ) : error ? (
+        <p className="query-error">A server side error occurred</p>
+      ) : data ? (
+        <>
+          <p
+            className="query-response"
+            dangerouslySetInnerHTML={{ __html: data.response }}
+          />
+          <p className="query-time text-xs">
+            Responded in {ms(data.timeTaken)}
+          </p>
+        </>
+      ) : null}
+    </>
+  )
+}
+
+export default ResourcesQuery
