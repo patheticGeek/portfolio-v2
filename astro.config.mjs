3b3e9963bfaba6c52445dcdaef63e532b8680951
Pathetic Geek <geekpathetic@gmail.com>
2025-07-23 05:42:01 +0530

    upgrade astro & plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
+import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/serverless'
-import react from '@astrojs/react'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
