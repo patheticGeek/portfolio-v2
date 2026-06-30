ccb29095aef8030d05c8e407612a981005535ac0
Pathetic Geek <geekpathetic@gmail.com>
2026-06-30 07:03:01 +0530

    update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
